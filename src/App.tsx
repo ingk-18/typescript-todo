436aa81aa3c8f285876fef65b6b9076e7abf6320
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-06-04 16:51:51 +0900

    環境構築・フォーム準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // inputValueという変数にsetInputValue関数を使って値を詰める
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setInputValue(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // submitしてもリロードされないようにする
+    e.preventDefault();
+
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
+  const handleEdit = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h2>TodoList with Typescript</h2>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            className="inputText"
+          />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="text"
+                onChange={() => handleEdit()}
+                className="inputText"
+                value={todo.inputValue}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
